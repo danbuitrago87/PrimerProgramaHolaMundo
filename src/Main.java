e3efba8c4bf6a7adfc8f49e6414c4487e2ef685a
unknown <danbuitrago87@gmail.com>
2024-09-15 11:13:48 -0500

    PrimerProgramaJava

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("¡Concluí la aula 01 y ahora estoy sumergiéndome en Java!");
     }
+
 }
